bc2d52278951e92920fccef6f80e1cca6f4009b4
Dan Jackson <jackson.daniel365@gmail.com>
2020-03-07 23:06:03 +0000

    (WIP) Added user identification via protocol

--- back-end/courier/src/main/java/com/jsg/courier/wsendpoint/SocketHandler.java
+++ back-end/courier/src/main/java/com/jsg/courier/wsendpoint/SocketHandler.java
@@ -11,11 +11,13 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jsg.courier.datatypes.Message;
+import com.jsg.courier.datatypes.User;
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
 	
 	private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+	private List<User> userList = new CopyOnWriteArrayList<>();
 	private static final ObjectMapper objectMapper = new ObjectMapper();
 	
 	@Override
@@ -26,24 +28,49 @@ public class SocketHandler extends TextWebSocketHandler {
 	}
 	
 	@Override
-	public void afterConnectionEstablished(WebSocketSession session) {
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		String[] headers = session.getHandshakeHeaders().getFirst("sec-websocket-protocol").split(",");		
+		if(userList.contains(new User(headers[0], headers[1]))) {
+			System.out.println("Already connected.");
+			return;
+		}
 		sessions.add(session);
-		System.out.println("WebSocket connection established between server and session with ID: " + session.getHandshakeHeaders().getFirst("sec-websocket-protocol"));
+		userList.add(new User(headers[0], headers[1]));
+		System.out.println("WebSocket connection established between server and session with details: " + headers[0] + ", " + headers[1]);
+		getSessions(session);
 	}
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) {
+		String[] headers = session.getHandshakeHeaders().getFirst("sec-websocket-protocol").split(",");		
 		sessions.remove(session);
+		userList.remove(new User(headers[0], headers[1]));
 		System.out.println("WebSocket connection closed.");
 	}
 	
-	public void broadcast(Message message) throws Exception {
+	private void broadcast(Message message) throws Exception {
+		System.out.println("(broadcast)Session size is: " + sessions.size());
 		for(WebSocketSession session : sessions) {
-			if(session.getHandshakeHeaders().getFirst("sec-websocket-protocol").equals(message.getId())) {
+			String[] headers = session.getHandshakeHeaders().getFirst("sec-websocket-protocol").split(",");		
+			if(headers[0].equals(message.getId())) {
 				System.out.println("Not sending to session: " + session.getHandshakeHeaders().getFirst("sec-websocket-protocol"));
 				continue;
 			}
 			session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
 		}
+	}
+	
+	private void getSessions(WebSocketSession session) throws Exception {
+		String[] headers = session.getHandshakeHeaders().getFirst("sec-websocket-protocol").split(",");		
+		String json = "`[";
+		for(int i=0; i<sessions.size(); i++) {
+			json += (new TextMessage(objectMapper.writeValueAsString(new User(headers[0], headers[1])))).getPayload();
+			if(i != sessions.size() - 1) {
+				json += ",";
+			}
+		}
+		json += "]";
+		System.out.println(json);
+		session.sendMessage(new TextMessage(json));
 	}
 }
--- front-end/src/components/ChatInfo.jsx
+++ front-end/src/components/ChatInfo.jsx
@@ -4,7 +4,7 @@ function ChatInfo(props) {
     // <input type="text" name="username" onChange={(e) => props.handleUsernameChange(e.target.value)} />
     return(
         <div>
-            <h1>AVATAR HERE</h1>
+            <h1>{props.currentChat.name}</h1>
             <form onSubmit={props.handleConnect} style={{display: "grid", alignContent: "center"}}>
                 <div>
                     Username: &nbsp;
@@ -14,8 +14,7 @@ function ChatInfo(props) {
             </form>
             <h3>Members:</h3>
             <ul>
-                <li key={props.chat.members.sender}>{props.chat.members.sender}</li>
-                {props.chat.members.receivers.map((receiver) => <li key={receiver}>{receiver}</li>)}
+                {props.currentChat.members.map((member) => <li key={member.sessionId}>{member.sessionId}</li>)}
             </ul>
         </div>
     );
