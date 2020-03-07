32d1d23466d166fd7044d1f36a2c5229f948202a
Dan Jackson <jackson.daniel365@gmail.com>
2020-03-07 12:32:41 +0000

    Added protocol ID, chat connect button [OGDEV-45]

--- front-end/src/components/ChatInfo.jsx
+++ front-end/src/components/ChatInfo.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function ChatInfo(props) {
+    // <input type="text" name="username" onChange={(e) => props.handleUsernameChange(e.target.value)} />
     return(
         <div>
             <h1>AVATAR HERE</h1>
-            Username: &nbsp;
-            <input type="text" name="username" onChange={(e) => props.handleUsernameChange(e.target.value)} />
+            <form onSubmit={props.handleConnect} style={{display: "grid", alignContent: "center"}}>
+                <div>
+                    Username: &nbsp;
+                    <input type="text" name="username"/>
+                </div>
+                <button> Connect to chat...</button>
+            </form>
             <h3>Members:</h3>
             <ul>
                 <li key={props.chat.members.sender}>{props.chat.members.sender}</li>
