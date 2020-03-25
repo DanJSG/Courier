d0012f34bf211c67cf778de84b3be4f5b2c0a2bf
Dan Jackson <jackson.daniel365@gmail.com>
2020-03-25 00:17:08 +0000

    Moved JWT into httponly cookie [OGDEV-58]

--- back-end/courier/src/main/java/com/jsg/courier/api/rest/JWTController.java
+++ back-end/courier/src/main/java/com/jsg/courier/api/rest/JWTController.java
@@ -2,10 +2,14 @@ package com.jsg.courier.api.rest;
 
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,19 +27,24 @@ public final class JWTController {
 
 	@CrossOrigin(origins = "http://localhost:3000/*")
 	@PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody ResponseEntity<String> createJwt(@RequestBody Map<String, Long> userId) {
+	public @ResponseBody ResponseEntity<String> createJwt(@RequestBody Map<String, Long> userId, HttpServletResponse response) {
 		Long id = userId.get("id");
 		if(id == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request failed.");
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(JWTHandler.createToken(id));	
+//		return ResponseEntity.status(HttpStatus.OK).body(JWTHandler.createToken(id));
+		
+		Cookie jwtCookie = new Cookie("jwt", JWTHandler.createToken(id));
+		jwtCookie.setHttpOnly(true);
+		response.addCookie(jwtCookie);
+		return ResponseEntity.status(HttpStatus.OK).body("Successfully created a new JWT.");
 	}
 	
 	@CrossOrigin(origins = "http://localhost:3000/*")
 	@PostMapping(value = "/verify")
-	public @ResponseBody ResponseEntity<String> verifyJwt(@RequestHeader(HttpHeaders.AUTHORIZATION) String auth) {
-		System.out.println(auth);
-		Boolean validity = JWTHandler.verifyToken(auth);
+	public @ResponseBody ResponseEntity<String> verifyJwt(@CookieValue String jwt) {
+		System.out.println(jwt);
+		Boolean validity = JWTHandler.verifyToken(jwt);
 		System.out.println(validity);
 		if(!validity) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to verify JWT.");
