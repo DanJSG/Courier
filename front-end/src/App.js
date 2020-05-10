4521fc2a45d812e4efe7bee02cac4919095d0971
Dan Jackson <jackson.daniel365@gmail.com>
2020-05-10 01:10:46 +0100

    Refactored front end, restructured project [OGDEV-88]

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import ChatPage from './components/ChatPage/ChatPage';
 import LandingPage from './components/LandingPage/LandingPage';
 import CallbackPage from './components/CallbackPage/CallbackPage';
+import {checkAuthorization} from './services/authprovider';
 import './App.scss';
 
 function App() {
@@ -10,48 +11,6 @@ function App() {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [id, setId] = useState(null);
   const [displayName, setDisplayName] = useState(null);
-
-  const checkAuthorization = async () => {
-    const lsToken = localStorage.getItem("acc.tok");
-    if(!lsToken) {
-      return {
-        authorized: false,
-        id: null,
-        displayName: null
-      };
-    };
-    return await fetch(`http://local.courier.net:8080/api/v1/verifyJwt`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Authorization": `Bearer ${lsToken}`
-      }
-    })
-    .then((response) => {
-      console.log("Recieved response...");
-      console.log(response);
-      if(response.status !== 200) {
-        return {
-          authorized: false,
-          id: null,
-          displayName: null
-        };
-      }
-      return response.json();
-    })
-    .then((json) => {
-      return {
-        authorized: true,
-        id: json.id,
-        displayName: json.displayName
-      }
-    })
-    .catch((error) => {
-      console.log("fetch error...");
-      console.log(error);
-      return false;
-    })
-  }
 
   const awaitAuthCheck = async () => {
     const result = await checkAuthorization();
