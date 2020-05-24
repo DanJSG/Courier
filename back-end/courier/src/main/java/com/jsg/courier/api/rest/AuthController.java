8c6addb9d93b48a83bf22dca5fef990a3debf479
Dan Jackson <jackson.daniel365@gmail.com>
2020-05-24 02:11:40 +0100

    Replaced DB methods with rest API call for user info [OGDEV-93]

--- back-end/courier/src/main/java/com/jsg/courier/api/rest/AuthController.java
+++ back-end/courier/src/main/java/com/jsg/courier/api/rest/AuthController.java
@@ -1,9 +1,16 @@
 package com.jsg.courier.api.rest;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -17,6 +24,7 @@ import com.jsg.courier.datatypes.UserInfo;
 import com.jsg.courier.repositories.UserInfoRepository;
 import com.jsg.courier.utilities.AuthHeaderHandler;
 import com.jsg.courier.utilities.JWTHandler;
+import com.mongodb.util.JSON;
 
 @RestController
 public class AuthController extends ApiController {
@@ -33,17 +41,56 @@ public class AuthController extends ApiController {
 	public @ResponseBody ResponseEntity<String> authorize(@CookieValue(name = ACCESS_TOKEN_NAME, required = false) String jwt, 
 			@RequestHeader String authorization) throws Exception {
 		String headerJwt = AuthHeaderHandler.getBearerToken(authorization);
+		System.out.println(headerJwt);
+		System.out.println(jwt);
 		if(!JWTHandler.tokenIsValid(jwt, ACCESS_TOKEN_SECRET) || 
 				!JWTHandler.tokenIsValid(headerJwt, ACCESS_TOKEN_SECRET)) {
 			return UNAUTHORIZED_HTTP_RESPONSE;
 		}
-		UserInfoRepository repo = new UserInfoRepository(SQL_CONNECTION_STRING, SQL_USERNAME, SQL_PASSWORD);
-		long id = JWTHandler.getIdFromToken(headerJwt);
-		List<UserInfo> infoList = repo.findWhereEqual("id", id, 1);
-		if(infoList == null || infoList.size() < 1) {
-			return UNAUTHORIZED_HTTP_RESPONSE;
+		
+		long id2 = JWTHandler.getIdFromToken(headerJwt);
+		URL url = new URL("http://local.courier.net:8090/api/v1/userInfo?client_id=ThpDT2t2EDlO&id=" + id2);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty(HttpHeaders.CONTENT_TYPE,"application/json");
+        connection.setRequestProperty(HttpHeaders.ACCEPT, "application/json");
+		connection.setRequestProperty(HttpHeaders.COOKIE, ACCESS_TOKEN_NAME + "=" + jwt);
+		System.out.println(authorization);
+		connection.setRequestProperty(HttpHeaders.AUTHORIZATION, authorization);
+		connection.setRequestMethod("GET");
+		connection.setConnectTimeout(15000);
+		connection.setReadTimeout(15000);
+		int status = connection.getResponseCode();
+		System.out.println(status);
+		InputStream stream;
+		if(status > 299) {
+			stream  = connection.getErrorStream();
+		} else {
+			stream = connection.getInputStream();
 		}
-		return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writeValueAsString(infoList.get(0)));
+		String responseBody = null;
+		if(stream != null) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+			String inputLine;
+			StringBuffer content = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+			    content.append(inputLine);
+			}
+			responseBody = content.toString();
+			in.close();
+			System.out.println("Response body is:");
+			System.out.println(responseBody);	
+		} else {
+			System.out.println("No error body");
+		}
+		connection.disconnect();
+		
+//		UserInfoRepository repo = new UserInfoRepository(SQL_CONNECTION_STRING, SQL_USERNAME, SQL_PASSWORD);
+//		long id = JWTHandler.getIdFromToken(headerJwt);
+//		List<UserInfo> infoList = repo.findWhereEqual("id", id, 1);
+//		if(infoList == null || infoList.size() < 1) {
+//			return UNAUTHORIZED_HTTP_RESPONSE;
+//		}
+		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
 	}
 
 }
