b541c1fdad6b9acfb6793360c8f2385acb3a39e7
Dan Jackson <jackson.daniel365@gmail.com>
2020-05-24 15:13:35 +0100

    Added classes for handling HTTP requests and responses [OGDEV-93]

--- back-end/courier/src/main/java/com/jsg/courier/api/rest/AuthController.java
+++ back-end/courier/src/main/java/com/jsg/courier/api/rest/AuthController.java
@@ -5,13 +5,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +18,12 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jsg.courier.datatypes.UserInfo;
-import com.jsg.courier.repositories.UserInfoRepository;
+import com.jsg.courier.httprequests.HttpRequestBuilder;
+import com.jsg.courier.httprequests.HttpResponse;
 import com.jsg.courier.utilities.AuthHeaderHandler;
 import com.jsg.courier.utilities.JWTHandler;
-import com.mongodb.util.JSON;
+
+import jdk.jfr.ContentType;
 
 @RestController
 public class AuthController extends ApiController {
@@ -48,41 +47,53 @@ public class AuthController extends ApiController {
 			return UNAUTHORIZED_HTTP_RESPONSE;
 		}
 		
-		long id2 = JWTHandler.getIdFromToken(headerJwt);
-		URL url = new URL("http://local.courier.net:8090/api/v1/userInfo?client_id=ThpDT2t2EDlO&id=" + id2);
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty(HttpHeaders.CONTENT_TYPE,"application/json");
-        connection.setRequestProperty(HttpHeaders.ACCEPT, "application/json");
-		connection.setRequestProperty(HttpHeaders.COOKIE, ACCESS_TOKEN_NAME + "=" + jwt);
-		System.out.println(authorization);
-		connection.setRequestProperty(HttpHeaders.AUTHORIZATION, authorization);
-		connection.setRequestMethod("GET");
-		connection.setConnectTimeout(15000);
-		connection.setReadTimeout(15000);
-		int status = connection.getResponseCode();
-		System.out.println(status);
-		InputStream stream;
-		if(status > 299) {
-			stream  = connection.getErrorStream();
-		} else {
-			stream = connection.getInputStream();
-		}
-		String responseBody = null;
-		if(stream != null) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(stream));
-			String inputLine;
-			StringBuffer content = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-			    content.append(inputLine);
-			}
-			responseBody = content.toString();
-			in.close();
-			System.out.println("Response body is:");
-			System.out.println(responseBody);	
-		} else {
-			System.out.println("No error body");
-		}
-		connection.disconnect();
+		long id = JWTHandler.getIdFromToken(headerJwt);
+		HttpRequestBuilder requestBuilder = new HttpRequestBuilder("http://local.courier.net:8090/api/v1/userInfo");
+		requestBuilder.addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON.toString());
+		requestBuilder.addHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON.toString());
+		requestBuilder.addCookie(ACCESS_TOKEN_NAME, jwt);
+		requestBuilder.addHeader(HttpHeaders.AUTHORIZATION, authorization);
+		requestBuilder.addParameter("id", id);
+		requestBuilder.addParameter("client_id", CLIENT_ID);
+		HttpResponse response = new HttpResponse(requestBuilder.toHttpURLConnection());
+//		System.out.println(response.)
+		
+		
+//		URL url = new URL("http://local.courier.net:8090/api/v1/userInfo?client_id=ThpDT2t2EDlO&id=" + id2);
+//		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setRequestProperty(HttpHeaders.CONTENT_TYPE,"application/json");
+//        connection.setRequestProperty(HttpHeaders.ACCEPT, "application/json");
+//		connection.setRequestProperty(HttpHeaders.COOKIE, ACCESS_TOKEN_NAME + "=" + jwt);
+//		System.out.println(authorization);
+//		connection.setRequestProperty(HttpHeaders.AUTHORIZATION, authorization);
+//		connection.setRequestMethod("GET");
+//		connection.setConnectTimeout(15000);
+//		connection.setReadTimeout(15000);
+//		
+//		int status = connection.getResponseCode();
+//		System.out.println(status);
+//		InputStream stream;
+//		if(status > 299) {
+//			stream  = connection.getErrorStream();
+//		} else {
+//			stream = connection.getInputStream();
+//		}
+//		String responseBody = null;
+//		if(stream != null) {
+//			BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+//			String inputLine;
+//			StringBuffer content = new StringBuffer();
+//			while ((inputLine = in.readLine()) != null) {
+//			    content.append(inputLine);
+//			}
+//			responseBody = content.toString();
+//			in.close();
+//			System.out.println("Response body is:");
+//			System.out.println(responseBody);	
+//		} else {
+//			System.out.println("No error body");
+//		}
+//		connection.disconnect();
 		
 //		UserInfoRepository repo = new UserInfoRepository(SQL_CONNECTION_STRING, SQL_USERNAME, SQL_PASSWORD);
 //		long id = JWTHandler.getIdFromToken(headerJwt);
@@ -90,7 +101,7 @@ public class AuthController extends ApiController {
 //		if(infoList == null || infoList.size() < 1) {
 //			return UNAUTHORIZED_HTTP_RESPONSE;
 //		}
-		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+		return ResponseEntity.status(HttpStatus.OK).body(response.getBody());
 	}
 
 }
---
+++ back-end/courier/src/main/java/com/jsg/courier/httprequests/HttpRequestBuilder.java
@@ -0,0 +1,130 @@
+package com.jsg.courier.httprequests;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+
+public class HttpRequestBuilder {
+	
+	private List<String> parameters;
+	private Map<String, String> headers;
+	private List<String> cookies;
+	private String url;
+	private String method;
+	private Boolean useCache = true;
+	private int timeouts = 0;
+	private Boolean allowInput = true;
+	private Boolean allowOutput = false;
+	private Boolean followRedirects = true;
+	
+	public HttpRequestBuilder() {}
+	
+	public HttpRequestBuilder(String url) {
+		setUrl(url);
+	}
+	
+	public HttpRequestBuilder(URL url) {
+		setUrl(url.toString());
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public <V> void addParameter(String name, V value) {
+		parameters.add(name + "=" + value);
+	}
+	
+	public <V> void addParameters(Map<String, V> params) {
+		params.forEach((key, value) -> {
+			addParameter(key, value);
+		});
+	}
+	
+	public void addHeader(String header, String value) {
+		headers.put(header, value);
+	}
+	
+	public void addHeaders(Map<String, String> headers) {
+		headers.forEach(this.headers::putIfAbsent);
+	}
+	
+	public void addCookie(String cookie, String value) {
+		cookies.add(cookie + "=" + value + ";");
+	}
+	
+	public void addCookies(Map<String, String> cookies) {
+		cookies.forEach((key, value) -> {
+			addCookie(key, value);
+		});
+	}
+	
+	public void setRequestMethod(HttpMethod method) {
+		this.method = method.toString();
+	}
+	
+	public void shouldUseCache(Boolean bool) {
+		this.useCache = bool;
+	}
+	
+	public void setTimeouts(int msTimeout) {
+		this.timeouts = msTimeout;
+	}
+	
+	public void allowInput(Boolean bool) {
+		this.allowInput = bool;
+	}
+	
+	public void allowOutput(Boolean bool) {
+		this.allowOutput = bool;
+	}
+	
+	public void shouldFollowRedirects(Boolean bool) {
+		this.followRedirects = bool;
+	}
+	
+	public HttpURLConnection toHttpURLConnection() throws Exception {
+		if(this.url == null || this.method == null) {
+			throw new Exception();
+		}
+		String urlWithParamsString = url;
+		Boolean firstParamAdded = false;
+		if(parameters != null && parameters.size() > 0) {
+			for(String param : parameters) {
+				if(!firstParamAdded) {
+					urlWithParamsString += "?" + param;
+					firstParamAdded = true;
+				} else {
+					urlWithParamsString += "&" + param;
+				}
+			}
+		}
+		URL urlWithParams = new URL(urlWithParamsString);
+		HttpURLConnection conn = (HttpURLConnection)urlWithParams.openConnection();
+		conn.setRequestMethod(method);
+		conn.setConnectTimeout(timeouts);
+		conn.setReadTimeout(timeouts);
+		conn.setDoInput(allowInput);
+		conn.setDoOutput(allowOutput);
+		conn.setUseCaches(useCache);
+		conn.setInstanceFollowRedirects(followRedirects);
+		if(headers != null && headers.size() > 0) {
+			headers.forEach((key, value) -> {
+				conn.setRequestProperty(key, value);
+			});
+		}
+		if(cookies != null && cookies.size() > 0) {
+			String cookiesString = "";
+			for(String cookie : cookies) {
+				cookiesString += cookie;
+			}
+			conn.setRequestProperty(HttpHeaders.COOKIE, cookiesString);
+		}
+		return conn;
+	}
+	
+}
