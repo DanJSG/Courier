7d8e2d2278f3232081694572d9a6d47017e0bb60
Dan Jackson <jackson.daniel365@gmail.com>
2020-06-23 00:18:23 +0100

    Implemented scrolling chat list, added chat click placeholder in front end [OGDEV-98]

--- front-end/src/components/ChatPage/Chats/ChatList.jsx
+++ front-end/src/components/ChatPage/Chats/ChatList.jsx
@@ -9,7 +9,7 @@ function ChatList(props) {
     }
 
     return (
-        <div>
+        <div className="d-flex flex-column h-100 mh-100">
             <div className="text-center">
                 <h1 className="">Chats</h1>
                 <div className="row pb-2 justify-content-center">
@@ -17,11 +17,13 @@ function ChatList(props) {
                     <button onClick={onCreateChatClicked} className="btn rounded-circle ml-1 mr-1 pl-2 pr-2"><i className="fa fa-plus"/></button>
                 </div>
             </div>
-            <ul className="list-group border-top">
-                {props.chats.map((chat) => (
-                    <ChatPicker name={chat.name} key={chat.id}></ChatPicker>
-                ))}
-            </ul>
+            <div className="col mh-100 h-100 flex-grow-1 overflow-auto">
+                    <ul className="list-group border-top">
+                        {props.chats.map((chat) => (
+                            <ChatPicker name={chat.name} key={chat.id}></ChatPicker>
+                        ))}
+                    </ul>
+            </div>
         </div>
     );
 }
--- front-end/src/components/ChatPage/Chats/ChatPicker.jsx
+++ front-end/src/components/ChatPage/Chats/ChatPicker.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function ChatPicker(props) {
+
+    const onChatClick = () => {
+        console.log(props.name);
+        // switch chats when this chat is clicked
+    }
+
     return(
-        <li className="list-group-item border-left border-right border-light rounded-0 chat-hover">{props.name}</li>
+        <li onClick={onChatClick} className="list-group-item border-left border-right border-light rounded-0 chat-hover">{props.name}</li>
     );
 }
 
