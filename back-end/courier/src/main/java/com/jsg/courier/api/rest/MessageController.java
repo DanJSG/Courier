fd44a218095228940f302fc02956d6479ad2b765
Dan Jackson <jackson.daniel365@gmail.com>
2020-06-28 19:08:47 +0100

    Implemented REST endpoint for getting chat history of specific chat [OGDEV-98]

--- back-end/courier/src/main/java/com/jsg/courier/api/rest/MessageController.java
+++ back-end/courier/src/main/java/com/jsg/courier/api/rest/MessageController.java
@@ -1,15 +1,23 @@
 package com.jsg.courier.api.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jsg.courier.constants.OAuth2;
+import com.jsg.courier.datatypes.Message;
+import com.jsg.courier.repositories.MessageRepository;
 
+@RestController
 public class MessageController extends ApiController {
 
 	@Autowired
@@ -26,7 +34,19 @@ public class MessageController extends ApiController {
 	@GetMapping(value = "/messages/getAll")
 	public ResponseEntity<String> getAll(@CookieValue(name = OAuth2.ACCESS_TOKEN_NAME, required = false) String jwt, 
 			@RequestHeader String authorization, @RequestParam String chatId) {
-		return UNAUTHORIZED_HTTP_RESPONSE;
+		if(!tokensAreValid(authorization, jwt)) {
+			return UNAUTHORIZED_HTTP_RESPONSE;
+		}
+		MessageRepository repo = new MessageRepository();
+		try {
+			List<Message> messages = repo.findAll(chatId);
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonResponse = mapper.writeValueAsString(messages);
+			return ResponseEntity.status(HttpStatus.OK).body(jsonResponse);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return INTERNAL_SERVER_ERROR_HTTP_RESPONSE;
+		}
 	}
 	
 }
