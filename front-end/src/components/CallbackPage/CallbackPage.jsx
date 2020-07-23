eca9b088c71ce950f9cbaf514ef01d4031e14ef6
Dan Jackson <jackson.daniel365@gmail.com>
2020-07-23 23:24:32 +0100

    Refactored all callbackservice.js methods into CallbackPage [OGDEV-108]

--- front-end/src/components/CallbackPage/CallbackPage.jsx
+++ front-end/src/components/CallbackPage/CallbackPage.jsx
@@ -1,12 +1,27 @@
 import {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
-import {requestTokens} from './services/callbackservice';
+// import {requestTokens} from './services/callbackservice';
 import {getQueryStringAsJson} from '../../services/querystringmanipulator';
+import {requestAccessToken, requestRefreshToken} from '../../services/authprovider'
 
 function CallbackPage() {
 
     const location = useLocation();
     const [params] = useState(getQueryStringAsJson(location));
+
+    const requestTokens = async (params, state, code_verifier) => {
+        const fetchedRefreshToken = await requestRefreshToken(params, state, code_verifier);
+        if(!fetchedRefreshToken) {
+            // TODO add redirect to 401 page or something similar
+            return;
+        }
+        const fetchedAccessToken = await requestAccessToken();
+        if(!fetchedAccessToken) {
+            // TODO add redirect to 401 page or something similar
+            return;
+        }
+        window.location.href = "http://local.courier.net:3000";
+    }
 
     useEffect(() => {
         const state = sessionStorage.getItem("auth.state");
