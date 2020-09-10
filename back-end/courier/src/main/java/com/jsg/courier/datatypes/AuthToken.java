79193448867efeb4cc98c5a55b16a45a77c505c6
DanJSG <jackson.daniel365@gmail.com>
2020-09-10 01:28:12 +0100

    Added AuthToken class and refactored associated methods within [OGDEV-124]

---
+++ back-end/courier/src/main/java/com/jsg/courier/datatypes/AuthToken.java
@@ -0,0 +1,54 @@
+package com.jsg.courier.datatypes;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class AuthToken {
+	
+	private String token;
+	private long id;
+	private String name;
+	
+	public AuthToken(String token) {
+		this.token = token;
+		id = extractId();
+		name = extractName();
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public boolean verify(String secret) {
+		Algorithm algorithm = Algorithm.HMAC256(secret);
+		JWTVerifier verifier = JWT.require(algorithm).build();
+		try {
+			verifier.verify(token);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private long extractId() {
+		DecodedJWT jwt = JWT.decode(token);
+		return jwt.getClaim("id").asLong();
+	}
+	
+	private String extractName() {
+		DecodedJWT jwt = JWT.decode(token);
+		return jwt.getClaim("name").asString();
+	}
+	
+}
